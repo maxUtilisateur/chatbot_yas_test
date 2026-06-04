b7b5faac43f754c4ce9c55b7efe5057ce88c22b3
maxUtilisateur <m45990635@gmail.com>
2026-06-04 16:09:51 +0000

    fr

--- src/config/data_source.ts
+++ src/config/data_source.ts
@@ -1,23 +1,4 @@
 // import 'reflect-metadata';
-// import { DataSource } from 'typeorm';
-
-// export const AppDataSource = new DataSource({
-//   type: 'postgres',
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-//   synchronize: true,
-//   logging: false,
-//   entities: [__dirname + '/../entities/*.ts'],
-// });
-
-
-// import 'reflect-metadata';
-// import { DataSource } from 'typeorm';
-
-// export const AppDataSource = new DataSource({
 //   type: 'postgres',
 //   url: process.env.DATABASE_URL, // ← Supabase URL
 //   synchronize: true,
